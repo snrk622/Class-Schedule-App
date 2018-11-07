f7dfae5c3ad91902b04c449a8b7c301587ee4ec1
snrk622 <s.0622.r@icloud.com>
2018-11-07 19:40:58 +0900

    Initial Commit

---
+++ classScheduleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  classScheduleApp
+//
+//  Created by 篠原立樹 on 2018/11/07.
+//  Copyright © 2018 Ostrich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
